b73580cedbc77ec76a2bd82c51c5e2a980807906
Hafsa Ahmad <hafsa.ahmad@kcl.ac.uk>
2022-10-17 23:01:36 +0100

    cast the list to strings

--- frequencies.py
+++ frequencies.py
@@ -1,12 +1,13 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
-list = [1,2,3,4]
+list = ['0', 4,4,'4','d','d','e',0,'a','d','4']
 
 def frequencies(items):
     frequencies = {}
     # Your code goes here
     for item in items:
+        item = str(item)
         if item in frequencies:
             frequencies[item] += 1
         else:
