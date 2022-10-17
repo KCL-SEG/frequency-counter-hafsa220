a4892f1cc4a1abfeacbded87ff1d48d918d69640
Hafsa Ahmad <hafsa.ahmad@kcl.ac.uk>
2022-10-17 22:58:35 +0100

    complete solution

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,19 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+list = [1,2,3,4]
+
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for item in items:
+        if item in frequencies:
+            frequencies[item] += 1
+        else:
+            frequencies[item] = 1
+
+    print(frequencies)
+
     return frequencies
+
+frequencies(list)
